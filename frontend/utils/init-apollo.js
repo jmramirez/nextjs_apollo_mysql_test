0f75586cab73a5a9f30c9451f87d602b2ceba695
jmramirez <josemramirez81@outlook.com>
2019-07-14 02:14:12 -0400

    added graphql setup files

---
+++ frontend/utils/init-apollo.js
@@ -0,0 +1,32 @@
+import { ApolloCLient, InMemoryCache, HttpLink } from 'apollo-boost'
+import fetch from 'isomorphic-unfetch'
+
+let apolloClient = null
+
+function create(initialState){
+  
+  const isBrowser = typeof window !== 'undefined'
+
+  return new ApolloCLient({
+    connectToDvTools: isBrowser,
+    ssrMode: !isBrowser,
+    link: new HttpLink({
+      uri: isBrowser? 'http://localhost:4000' : 'http://backend:4000',
+      credentials: 'same-origin',
+      fetch: !isBrowser && fetch,
+    }),
+    cache: new InMemoryCache().restore(initialState || {}),
+  })
+}
+
+export default function initApollo(initialState){
+  if(typeof window === 'undefined'){
+    return create(initialState)
+  }
+
+  if(!apolloClient){
+    apolloClient = create(initialState)
+  }
+
+  return apolloClient
+}
